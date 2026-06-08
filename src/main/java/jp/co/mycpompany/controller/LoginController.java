4cfe210ee268216db9b09df1c8d20270f9c148f9
masakazu-ishida <mishida@support-s-frontier.com>
2026-06-08 23:07:15 +0900

    あああ

--- src/main/java/jp/co/mycpompany/controller/LoginController.java
+++ src/main/java/jp/co/mycpompany/controller/LoginController.java
@@ -15,9 +15,6 @@ import jp.co.mycpompany.service.AuthenticateService;
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public LoginController() {
 		super();
 		// TODO Auto-generated constructor stub
